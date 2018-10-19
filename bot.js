d702d21446dea73f091e59a85818bfc4494999dc
magicbowen <e.bowen.wang@icloud.com>
2018-10-19 14:35:03 +0800

    add list template

--- bot.js
+++ bot.js
@@ -27,6 +27,42 @@ class Bot extends BaseBot {
 
         this.addIntentHandler('ai.dueros.common.default_intent', () => {
             this.waitAnswer()
+            if (request.getQuery().indexOf('测试列表') != -1) {
+                const list = [
+                    {
+                        course : '数学',
+                        time   : '9:00 ~ 10:00',
+                        address: '学二楼3楼304',
+                        teacher: '杨老师'
+                    },
+                    {
+                        course : '语文',
+                        time   : '10:00 ~ 11:00',
+                        address: '学二楼3楼304'
+                    },
+                    {
+                        course : '音乐',
+                        time   : '11:00 ~ 12:00'
+                    },
+                    {
+                        course : '外语'
+                    },
+                    {
+                        course : '晚自习'
+                    },
+                    {
+                        course : '芭蕾课',
+                        time   : '9:30 ~ 10:30',
+                        address: '上海市浦东新区金科路长泰广场A座308',
+                        teacher: '雷老师'
+
+                    }
+                ]
+                return {
+                    directives: [this.getListTemplate(list)],
+                    outputSpeech: '列表显示如上，您满意了吗？'
+                }
+            }            
             var that = this
             return chatbot.replyToText(user_id, request.getQuery(), user_context)
                           .then((result) => { return that.getQrcodeImageUrl(user_id, result)})
@@ -74,52 +110,26 @@ class Bot extends BaseBot {
             return {outputSpeech: result.reply}
         }
 
-        // if (this.shouldRedirectForDisplay(result)) {
-        //     let reply = `推荐您使用微信扫码录入课程。或者直接对我说出您${result.data[0].time}要上的全部课程名称？`
-        //     return {
-        //         directives: [this.getTemplateWithoutCourse(reply, result.image)],
-        //         outputSpeech: reply
-        //     }
-        // }
-
         if (this.shouldDisplayQrcode(result)) {
             let reply = '请使用微信扫描二维码，打开小程序进行课程的录制和修改。'
             return {
-                directives: [this.getTemplateWithoutCourse(reply, result.image)],
+                directives: [this.getTextTemplateWithImage(reply, result.image)],
                 outputSpeech: reply
             }
         }
 
-        if (this.shouldFindDarwin(result)) {
-            let reply = '请使用微信扫描二维码，打开小程序进行课程的录制和修改。'
-            return {
-                directives: [this.getTemplateWithoutCourse(reply, result.image)],
-                outputSpeech: '请使用微信扫描二维码，打开小程序进行课程的录制和修改。'
-            }
-        }
-
         return {
-            directives: [this.getTextTeplate(result.reply)],
+            directives: [this.getTextTemplate(result.reply)],
             outputSpeech: result.reply
         }
     }
 
-    shouldRedirectForDisplay(result) {
-        if (!this.isSupportDisplay()) return false
-        return ((result.data)&&(result.data[0].intent === 'record-course'))
-    }
-
     shouldDisplayQrcode(result) {
         if (!this.isSupportDisplay()) return false
-        return (result.intent.indexOf('how-to-record') != -1)
+        return ((result.intent.indexOf('how-to-record') != -1)||(result.reply.indexOf('哒尔文') != -1))
     }
 
-    shouldFindDarwin(result) {
-        if (!this.isSupportDisplay()) return false
-        return (result.reply.indexOf('哒尔文') != -1)
-    }
-    
-    getTextTeplate(text) {
+    getTextTemplate(text) {
         let bodyTemplate = new BaseBot.Directive.Display.Template.BodyTemplate1();
         bodyTemplate.setTitle('课程表');
         bodyTemplate.setPlainTextContent(text);
@@ -128,13 +138,30 @@ class Bot extends BaseBot {
         return renderTemplate;
     }
 
-    getTemplateWithoutCourse(text, image) {
+    getTextTemplateWithImage(text, image) {
         let bodyTemplate = new BaseBot.Directive.Display.Template.BodyTemplate2();
         bodyTemplate.setTitle('课程表');
         bodyTemplate.setPlainContent(text);
         bodyTemplate.setImage(image, 100, 100);
         bodyTemplate.setBackGroundImage(config.background);
         let renderTemplate = new BaseBot.Directive.Display.RenderTemplate(bodyTemplate);
+        return renderTemplate;
+    }
+
+    getListTemplate(list) {
+        let listTemplate = new BaseBot.Directive.Display.Template.ListTemplate2();
+        listTemplate.setTitle('课程表');
+        listTemplate.setBackGroundImage(config.background);
+        for (let item of list) {
+            let listItem = new BaseBot.Directive.Display.Template.ListTemplateItem();
+            // listItem.setImage('https://skillstore.cdn.bcebos.com/icon/100/c709eed1-c07a-be4a-b242-0b0d8b777041.jpg');
+            listItem.setPlainPrimaryText(item.course);  
+            listItem.setPlainSecondaryText(item.time);
+            listItem.setPlainTertiaryText(item.teacher + '       ' + item.address);
+            
+            listTemplate.addItem(listItem);
+        }
+        let renderTemplate = new BaseBot.Directive.Display.RenderTemplate(listTemplate);
         return renderTemplate;
     }
 }
